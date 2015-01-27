50a5850f0cd0d64eac6040a5bbdac625d4c2b102
Jacob Case <jacobcase94@gmail.com>
2015-01-26 21:28:10 -0600

    routing skeleton

--- banking/banking/views.py
+++ banking/banking/views.py
@@ -1,3 +1,45 @@
 from banking import app
 
+@app.route('/cgi-bin/actions/create-user')
+def create_user():
+    pass
 
+@app.route('/cgi-bin/actions/delete-user')
+def delete_user():
+    pass
+
+@app.route('/cgi-bin/actions/find-user')
+def find_user():
+    pass
+
+@app.route('/cgi-bin/actions/get-access-token')
+def get_access_token():
+    pass
+
+@app.route('/cgi-bin/actions/get-admin-access-token')
+def get_admin_token():
+    pass
+
+@app.route('/cgi-bin/actions/logout')
+def logout():
+    pass
+
+@app.route('/cgi-bin/actions/make-deposit')
+def make_deposit():
+    pass
+
+@app.route('/cgi-bin/actions/make-payment')
+def make_payment():
+    pass
+
+@app.route('/cgi-bin/actions/make-withdrawal')
+def make_payment():
+    pass
+
+@app.route('/cgi-bin/show/landing')
+def landing():
+    pass
+
+@app.route('/cgi-bin/show/show-user')
+def show_user():
+    pass
