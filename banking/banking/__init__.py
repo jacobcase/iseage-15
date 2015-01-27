784e8c1594760f9bc475ff2771b923a588d96e3d
Jacob Case <jacobcase94@gmail.com>
2015-01-27 15:43:03 -0600

    added some views content and templates

--- banking/banking/__init__.py
+++ banking/banking/__init__.py
@@ -1,6 +1,10 @@
 import logging
 from flask import Flask
 
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s %(module)s: %(message)s",
+    level="INFO"
+)
 logging.info("Creating flask app")
 app = Flask(__name__, static_url_path='')
 app.debug = True
--- banking/banking/views.py
+++ banking/banking/views.py
@@ -1,3 +1,5 @@
+from flask import render_template, request, session, url_for, redirect
+
 from banking import app
 import os.path
 
@@ -27,7 +29,9 @@ def get_admin_token():
 
 @app.route('/cgi-bin/actions/logout')
 def logout():
-    pass
+    # remove users cookie, delete their session from the database
+    # all that security stuff
+    return redirect(url_for("landing"))
 
 @app.route('/cgi-bin/actions/make-deposit')
 def make_deposit():
@@ -43,8 +47,26 @@ def make_withdrawal():
 
 @app.route('/cgi-bin/show/landing')
 def landing():
-    pass
+    user = "cdc" # replace
+    #check the auth
+    authenticated = True
+    if not authenticated:
+        return render_template("not-logged-in.html")
+    else:
+        return render_template("welcome.html", USER_NAME=user)
+
 
 @app.route('/cgi-bin/show/show-user')
 def show_user():
-    pass
+    user = "cdc"
+    balance = "100"
+    tansactions = []
+    authenticated = True
+
+    if not authenticated:
+        return ("Status: 403\nForbidden!", 403)
+    
+    if not user:
+        return render_template("user-not-found.html")
+
+    return render_template("user.html", USER_NAME=user, BALANCE=balance)
