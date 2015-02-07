199660c4f0e3044b51f924d56f0769658cc174c0
madmanali93 <ali.bu3alwa@gmail.com>
2015-02-06 23:55:54 -0600

    website deploys

--- banking/banking/tools/flag.py
+++ banking/banking/tools/flag.py
@@ -3,8 +3,8 @@ import yaml
 import argparse
 from banking.db import DB, User, Transaction
 
-parser = argparse.ArgumentParse()
-parse.add_argument("-f", "--flag", type=str)
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--flag", type=str)
 
 args, unknown = parser.parse_known_args()
 
